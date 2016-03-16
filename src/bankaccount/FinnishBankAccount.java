32e3d5eceea4c254f86ad672e96a2e4ca9b9e60c
Solid <Solid@86.50.49.221>
2016-03-16 22:28:38 +0200

    contains-method created and if-else structure for group division added.

--- src/bankaccount/FinnishBankAccount.java
+++ src/bankaccount/FinnishBankAccount.java
@@ -6,24 +6,50 @@ joel.salminen@gmail.com
 package bankaccount;
 
 public class FinnishBankAccount {
-    int accountNumber;
-    String sAccountNumber;
+    String accountNumber;
+    int[] groupA = {1,2,31,33,34,36,37,38,39};
+    int[] groupB = {4,5,6,8};
     
     public FinnishBankAccount(String aNumber){
-        sAccountNumber = aNumber;
+        accountNumber = aNumber;
         checkValidity();
     }
     private void checkValidity(){
-        /* Checks if the given bank account number is valid */
+        /* checking if the given bank account number is valid */
         
-        /* check if the input lenght is between 9-15*/
-        if (sAccountNumber.length()<9 || sAccountNumber.length() > 15){
+        /* Checking if the input lenght is between 9-15*/
+        if (accountNumber.length()<9 || accountNumber.length() > 15){
             System.out.println("Virhe: pituus");
         }
         
-        /* checks if the 7th character of the input is a dash */
-        if (sAccountNumber.charAt(6) != '-'){
+        /* Checking if the 7th character of the input is a dash */
+        if (accountNumber.charAt(6) != '-'){
             System.out.println("Virhe: väliviiva");
         }
+        
+        /* Transforming the bank account number into machine code: */
+        
+        /* tarkistaa mihin ryhmään tää numero tungetaan*/
+        if (contains(groupA, Integer.parseInt(accountNumber.substring(0,1))) 
+                || contains(groupA, Integer.parseInt(accountNumber.substring(0,2)))){
+            System.out.println("ryhmä A");
+        }
+        else if (contains(groupB, Integer.parseInt(accountNumber.substring(0,1)))){
+            System.out.println("ryhmä B");
+        }
+        else{
+            System.out.println("virhe: alku");
+        }
+
+    }
+    
+    /* Checks if an array contains a certain integer value*/
+    private boolean contains(int[] intArray, int value){
+        for (int i : intArray){
+            if (value == i){
+                return true;
+            }
+        }
+        return false;
     }
 }
