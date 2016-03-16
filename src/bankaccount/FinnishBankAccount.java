87246d3aa8bdc1d42e322ffc301f86e7aa6a870e
Solid <Solid@86.50.49.221>
2016-03-16 21:35:28 +0200

    checkValidity now checks if the 7th character of the given bank account number is a dash.

--- src/bankaccount/FinnishBankAccount.java
+++ src/bankaccount/FinnishBankAccount.java
@@ -7,13 +7,19 @@ package bankaccount;
 
 public class FinnishBankAccount {
     int accountNumber;
+    String sAccountNumber;
     
-    public FinnishBankAccount(int aNumber){
-        accountNumber = aNumber;
+    public FinnishBankAccount(String aNumber){
+        sAccountNumber = aNumber;
         checkValidity();
     }
     private void checkValidity(){
-        if (accountNumber == 1){
+        /* Checks if the given bank account number is valid */
+        
+        /* check if the input lenght is between 9-15*/
+        
+        /* checks if the 7th character of the input is "-"? */
+        if (sAccountNumber.charAt(6) != '-'){
             System.out.println("Virhe");
         }
     }
